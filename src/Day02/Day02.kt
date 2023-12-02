0a79d78e7b46fccc5951fc1f28a2dbb1a402e4b7
Tristan Fazio <20038487+tristanfazio@users.noreply.github.com>
2023-12-02 22:26:43 +1100

    day2

--- src/Day02/Day02.kt
+++ src/Day02/Day02.kt
@@ -2,34 +2,123 @@ package Day02
 
 import println
 import readInput
+import kotlin.math.min
 
-const val DAY = "02";
+const val DAY = "02"
 fun main() {
+    // 12 red cubes, 13 green cubes, and 14 blue cubes
+    var maxMap = mapOf(
+            Pair("red", 12),
+            Pair("green", 13),
+            Pair("blue", 14))
+
+    fun playGame(line: String): Int {
+        //split gameId, gameInput
+        //split rounds
+        val gameSplit = line.split("Game ", ": ", "; ").drop(1)
+        val gameId = gameSplit[0]
+        println("Game $gameId")
+        //for rounds
+        for (i in 1..<gameSplit.size) {
+            //check each R G B <= Max
+            val round = gameSplit[i]
+            print("Round: ")
+            round.println()
+
+            val blocks = round.split(",")
+
+            blocks.forEach { block ->
+                val blockSplit = block.trimStart().split(" ")
+                val num = blockSplit[0]
+                val colour = blockSplit[1]
+                num.println()
+                colour.println()
+                if (num.toInt() > maxMap.get(colour)!!) {
+                    //false? return 0
+                    return 0
+                }
+            }
+        }
+        return gameId.toInt()
+    }
+
+    fun playGame2(line: String): Int {
+        var minMap = mutableMapOf(
+                Pair("red", 0),
+                Pair("green", 0),
+                Pair("blue", 0))
+
+        //split gameId, gameInput
+        //split rounds
+        val gameSplit = line.split("Game ", ": ", "; ").drop(1)
+        val gameId = gameSplit[0]
+        println("Game $gameId")
+        //for rounds
+        for (i in 1..<gameSplit.size) {
+            //check each R G B <= Max
+            val round = gameSplit[i]
+            print("Round: ")
+            round.println()
+
+            val blocks = round.split(",")
+
+            blocks.forEach { block ->
+                val blockSplit = block.trimStart().split(" ")
+                val num = blockSplit[0].toInt()
+                val colour = blockSplit[1]
+                if (num > minMap.get(colour)!!) {
+                    minMap[colour] = num
+                }
+            }
+        }
+        minMap.println()
+        return minMap["red"]!! * minMap["blue"]!! * minMap["green"]!!
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var idSum = 0
+        input.forEach { line ->
+            idSum += playGame(line)
+        }
+        return idSum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var powerSum = 0
+        input.forEach { line ->
+            val power = playGame2(line)
+            powerSum += power
+            println("Power: $power, PowerSum: $powerSum")
+        }
+        return powerSum
     }
 
-    val testInput = readInput("Day${Day01.DAY}/Day${Day01.DAY}_test")
+    val testInput = readInput("Day${DAY}/Day${DAY}_test")
 
     println("---------------Test Part1---------------")
     val testp1 = part1(testInput)
-    println("Test Part1 InputAnswer:")
-    testp1.println()
-    check(testp1 == 142)
+    check(testp1 == 8)
 
-    val input = readInput("Day${Day01.DAY}/Day${Day01.DAY}")
+    println("---------------Test Part2---------------")
+    val testp2 = part2(testInput)
+    check(testp2 == 2286)
+
+    val input = readInput("Day${DAY}/Day${DAY}")
 
     println("---------------Part1---------------")
     val p1 = part1(input)
-    println("Part1 InputAnswer:")
-    p1.println()
 
     println("---------------Part2---------------")
     val p2 = part2(input)
-    println("Part2 InputAnswer:")
+
+
+    println("=============================")
+    print("Test Part1 Answer: ")
+    testp1.println()
+    print("Test Part2 Answer: ")
+    testp2.println()
+    print("Part1 Answer: ")
+    p1.println()
+    print("Part2 Answer: ")
     p2.println()
 }
